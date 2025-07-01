1a833038efb2d1fe449ba3f26ba2224df7898c8f
ismail2489 <ismailofkir2001@gmail.com>
2025-07-01 20:30:49 +0200

    second verson

---
+++ main1.js
@@ -0,0 +1,26 @@
+alert("this is an alert")
+var visitorname =prompt("what's your name")
+console.log(visitorname)
+var isCoolWithIt = confirm("Are you ok with it?")
+console.log(isCoolWithIt )
+var num = 0;
+
+console.log(num)
+
+num ++
+
+console.log(num)
+
+num --
+
+console.log(num)
+var x = "Hello"
+var y = "World"
+console.log(x + y)
+function add(a, b) {
+
+	return a + b;
+
+}
+
+console.log(add( 5,2)); // prints 7
